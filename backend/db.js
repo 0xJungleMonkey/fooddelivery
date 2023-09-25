f1aab80757b35414d3a8419c2e6015b449f1fa24
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-09-25 14:51:32 -0400

    error handle

--- backend/db.js
+++ backend/db.js
@@ -2,6 +2,15 @@ const mongoose = require("mongoose");
 const config = require("./config.json");
 
 async function mongoDB() {
-  await mongoose.connect(config.mongodbconnection);
+  try {
+    await mongoose.connect(config.mongodbconnection, { useNewUrlParser: true });
+
+    console.log("connected");
+    const fetched_data = await mongoose.connection.db.collection("recipes");
+    const data = await fetched_data.find({}).toArray();
+    console.log(data);
+  } catch (err) {
+    console.error("Error:", err);
+  }
 }
 module.exports = mongoDB;
