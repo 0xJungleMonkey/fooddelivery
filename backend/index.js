92bea1b84025b50b6b6b3d1debbbd46823856846
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-09-29 12:57:39 -0400

    remove not used code

--- backend/index.js
+++ backend/index.js
@@ -10,15 +10,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.use(cors());
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 app.use(express.json());
 app.use("/api", require("./routes/createUser"));
 app.listen(port, () => {
