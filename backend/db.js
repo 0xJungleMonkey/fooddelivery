bd88de1ee50ff93c1e2b5f3fb755368239e3c0ce
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-10-01 15:18:54 -0400

    store menu info into global variable.

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,16 @@ const config = require("./config.json");
 async function mongoDB() {
   try {
     await mongoose.connect(config.mongodbconnection, { useNewUrlParser: true });
-
     console.log("connected");
+    //Storing the menu items and food categories in global variables (global.menu_items and global.category) suggests that this data is intended to be accessible throughout the application's lifecycle.
     const fetched_data = await mongoose.connection.db.collection("menu_items");
     const data = await fetched_data.find({}).toArray();
-    console.log(data);
+
+    const foodCategory = await mongoose.connection.db.collection("category");
+    const catData = await foodCategory.find({}).toArray();
+
+    global.menu_items = data;
+    global.category = catData;
   } catch (err) {
     console.error("Error:", err);
   }
--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/dishes", (req, res) => {
+router.post("/items", (req, res) => {
   try {
-    console.log(global.food_items);
-    res.send([global.food_items]);
+    console.log(global.menu_items);
+    res.send([global.menu_items]);
   } catch (error) {
     console.log(error.message);
     res.send("server error");
   }
 });
+module.exports = router;
