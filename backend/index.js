9c4716ff580f0270b69f1fbd7a05cf2ca4978c21
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-11 16:39:21 -0500

    use my code to test

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,33 @@
-// const express = require("express");
-// const app = express();
-// const port = 5001;
+const express = require("express");
+const app = express();
+const port = 5001;
 // const mongoDB = require("./db");
-// var cors = require("cors");
+var cors = require("cors");
 
 // mongoDB();
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 // app.use(cors());
 // app.use(express.json());
 // app.use("/api", require("./routes/user"));
 // app.use("/menu", require("./routes/menu"));
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-// module.exports = app;
-const express = require('express')
-const app = express()
-const PORT = 4000
-
-
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
+module.exports = app;
+// const express = require('express')
+// const app = express()
+// const PORT = 4000
 
-// Export the Express API
-module.exports = app
+// app.get('/home', (req, res) => {
+//   res.status(200).json('Welcome, your app is working well');
+// })
+
+// app.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+// // Export the Express API
+// module.exports = app
