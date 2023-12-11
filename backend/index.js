71e17f89096076b99fe1a3caf5d0bd544092affa
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-11 16:35:41 -0500

    test deployment with simple code

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,35 @@
-const express = require("express");
-const app = express();
-const port = 5001;
-const mongoDB = require("./db");
-var cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const port = 5001;
+// const mongoDB = require("./db");
+// var cors = require("cors");
 
-mongoDB();
+// mongoDB();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api", require("./routes/user"));
+// app.use("/menu", require("./routes/menu"));
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+// module.exports = app;
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
-app.use(cors());
-app.use(express.json());
-app.use("/api", require("./routes/user"));
-app.use("/menu", require("./routes/menu"));
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-module.exports = app;
+
+// Export the Express API
+module.exports = app
