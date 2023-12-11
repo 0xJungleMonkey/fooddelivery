497adf439621719a137f1504d7153db723412535
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-11 16:42:37 -0500

    2 use my code to test

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 5001;
-// const mongoDB = require("./db");
+const mongoDB = require("./db");
 var cors = require("cors");
 
-// mongoDB();
+mongoDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-// app.use(cors());
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 // app.use("/api", require("./routes/user"));
-// app.use("/menu", require("./routes/menu"));
+app.use("/menu", require("./routes/menu"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
