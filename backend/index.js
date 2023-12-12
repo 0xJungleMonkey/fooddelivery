a3602bc34864910da54b160dff5283af030754b2
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-12 17:21:15 -0500

    fixed cors error, place it front of all the routes

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,24 @@ const app = express();
 const port = 5001;
 var cors = require("cors");
 const { connectToMongoDB } = require("./db");
-connectToMongoDB();
-app.use(cors());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+connectToMongoDB()
+  .then(() => {
+    app.use(cors());
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
 
-app.use(express.json());
-app.get("/api", (req, res) => {
-  res.send("Hello World!");
-});
-app.use("/menu", require("./routes/menu"));
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    app.use(express.json());
+    app.get("/api", (req, res) => {
+      res.send("Hello World!");
+    });
+    app.use("/menu", require("./routes/menu"));
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
 module.exports = app;
