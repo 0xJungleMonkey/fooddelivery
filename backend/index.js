335f3a28d98c4e6f2413d7e36f4f83967478d3bc
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-12 17:30:50 -0500

    roll back

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,20 @@ const app = express();
 const port = 5001;
 var cors = require("cors");
 const { connectToMongoDB } = require("./db");
-connectToMongoDB()
-  .then(() => {
-    app.use(cors());
-    app.get("/", (req, res) => {
-      res.send("Hello World!");
-    });
+connectToMongoDB();
 
-    app.use(express.json());
-    app.get("/api", (req, res) => {
-      res.send("Hello World!");
-    });
-    app.use("/menu", require("./routes/menu"));
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(express.json());
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
+app.use("/menu", require("./routes/menu"));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = app;
