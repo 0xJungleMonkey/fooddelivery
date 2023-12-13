9c019367b228122a9779c5f15b78a5f34a98527a
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-13 15:43:44 -0500

    add the error handle message

--- backend/routes/menu.js
+++ backend/routes/menu.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { fetchDataFromMongoDB } = require("../db");
 
-router.post("/items", async (req, res) => {
+router.post("/items", (req, res) => {
   try {
     // Ensure that data is fetched before responding
-    await fetchDataFromMongoDB();
+    // await fetchDataFromMongoDB();
     res.send([global.menu_items, global.category]);
   } catch (error) {
     console.log(error.message);
