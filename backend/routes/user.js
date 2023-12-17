d87248ced3f737ea9389fd8267f9e09a49190b78
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-17 15:23:53 -0500

    test api

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -16,6 +16,7 @@ router.post(
   ).isLength({ min: 5 }),
   async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
+    console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
