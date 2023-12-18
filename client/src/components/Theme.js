59bdd9d67ef6555499a1fb4fdfb448f4f91e7f60
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-18 00:07:27 -0500

    optimize darkmode button

--- client/src/components/Theme.js
+++ client/src/components/Theme.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Darkmode from "darkmode-js";
+import { useState, useEffect } from "react";
+import { Moon, Sun } from "react-bootstrap-icons";
 function YourComponent() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
   const darkmode = new Darkmode();
   const handleDarkModeToggle = () => {
     darkmode.toggle();
+    isDarkMode ? setIsDarkMode(false) : setIsDarkMode(true);
   };
 
   return (
     <div>
       {/* Your component content */}
       <button id="your-darkmode-button-id" onClick={handleDarkModeToggle}>
-        Toggle Dark Mode
+        {isDarkMode ? <Sun /> : <Moon />}
       </button>
     </div>
   );
