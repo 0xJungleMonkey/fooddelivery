c136366eef0f81a9e8bc35df279cc0d52b062b7e
Monkey 0xJungle.com <Monkey@0xJungle.com>
2023-12-17 23:42:17 -0500

    update api link

--- client/src/screens/Myorder.js
+++ client/src/screens/Myorder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
   const fetchMyOrder = async () => {
     console.log(localStorage.getItem("userEmail"));
-    await fetch("http://localhost:5001/user/orders", {
+    await fetch("https://quickbite-htqe.onrender.com/user/orders", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: "POST",
